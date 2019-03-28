ff9224898fb54eee12cf1d9d12a6244f81c7022c
aluno <aluno@ubuntu-test>
2019-03-28 19:45:27 -0300

    Teste 20190328

--- js/aula-04.js
+++ js/aula-04.js
@@ -1,38 +1,6 @@
-var el = document.getElementById("titulo");
-console.log(el);
 
-function inserir()
-{
-    var lista = document.getElementById("ulLista");
-    var input = document.getElementById("texto");
+console.log("teste");
 
-    //var x = document.createElement("LI");
-    //var t = document.createTextNode(input.value);
-    //x.appendChild(t);
-    //lista.appendChild(x);
-    
-    var texto = "<li>" + input.value + "</li>";
-    
-    //var conteudo = lista.innerHTML;    
-    //if(!conteudo.includes(texto)) { //Suportado só apatir do ECMA6
-    
-    if(lista.innerHTML.search(texto) === -1) {
-        lista.innerHTML += texto;
-    } else {
-        window.alert("Texto ja cadastrado.");
-    }
-    
-    
-
-}
-
-function pesquisar() {
-    var input = document.getElementById("palavra");
-    var div = document.getElementById("dilma");
-    
-    var conteudo = div.innerHTML;
-    var novo = "<span style=\"background-color: coral;\">" + input.value + "</span>";
-    
-    div.innerHTML = conteudo.replace(input.value, novo);
-    
-}
+$(document).ready(function(){
+    alert("A");
+});
